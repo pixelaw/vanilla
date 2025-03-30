51f5518dc9434effdd3c6d5cf185d2c589019260
thiscaspar <thiscaspar@gmail.com>
2025-03-31 07:19:10 +0800

    work on interaction, emoji

--- src/index.tsx
+++ src/index.tsx
@@ -1,59 +1,71 @@
-import Main from "@/Main.tsx"
-import React from "react"
-import ReactDOM from "react-dom/client"
-import "@/index.css"
-import {PixelawProvider, usePixelawProvider} from "@pixelaw/react"
-import {BrowserRouter} from "react-router-dom"
-import {DojoEngine} from "@pixelaw/core-dojo"
-import {MudEngine} from "@pixelaw/core-mud"
+import Main from "@/Main.tsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "@/index.css";
+import {PixelawProvider, usePixelawProvider} from "@pixelaw/react";
+import {BrowserRouter} from "react-router-dom";
+import {DojoEngine} from "@pixelaw/core-dojo";
+import {MudEngine} from "@pixelaw/core-mud";
 import {getCoreDefaultsFromUrl, getWorldForUrl} from "@/utils.ts";
-import {StarknetChainProvider} from "@pixelaw/react-dojo"
-import {WorldsRegistry} from "@pixelaw/core";
-import worldsRegistry from "@/config/worlds.json"
+import {StarknetChainProvider} from "@pixelaw/react-dojo";
+import type {WorldsRegistry} from "@pixelaw/core";
+import worldsRegistry from "@/config/worlds.json";
 
 const AppContent = () => {
-    const { coreStatus } = usePixelawProvider()
+	const { coreStatus } = usePixelawProvider();
 
-    if (coreStatus == "error") {
-        return <div className="error-message">Error occurred, check the logs</div>
-    }
+	if (coreStatus === "error") {
+		return <div className="error-message">Error occurred, check the logs</div>;
+	}
 
-    return (
-        <BrowserRouter>
-            {coreStatus === 'ready' ? (
-                <StarknetChainProvider> <Main /></StarknetChainProvider>
-            ) : (
-                <div className="loading-message">Loading, pls wait 🧘</div>
-            )}
-        </BrowserRouter>
-    )
-}
+	return (
+		<BrowserRouter>
+			{coreStatus === "ready" ? (
+				<StarknetChainProvider>
+					{" "}
+					<Main />
+				</StarknetChainProvider>
+			) : (
+				<div className="loading-message">Loading, pls wait 🧘</div>
+			)}
+		</BrowserRouter>
+	);
+};
 
 const App = () => {
-    return (
-        <PixelawProvider worldsRegistry={worldsRegistry as WorldsRegistry} world={world} engines={engines} coreDefaults={coreDefaults}>
-            <AppContent />
-        </PixelawProvider>
-    )
-}
+	return (
+		<PixelawProvider
+			worldsRegistry={worldsRegistry as WorldsRegistry}
+			world={world}
+			engines={engines}
+			coreDefaults={coreDefaults}
+		>
+			<AppContent />
+		</PixelawProvider>
+	);
+};
 
 const { protocol, hostname } = window.location;
 const baseUrl = `${protocol}//${hostname}`;
 
-const world  = getWorldForUrl(worldsRegistry as WorldsRegistry, baseUrl, "local")
+const world = getWorldForUrl(
+	worldsRegistry as WorldsRegistry,
+	baseUrl,
+	"local",
+);
 
-const rootElement = document.getElementById("root")
+const rootElement = document.getElementById("root");
 
-const engines = {"dojo": DojoEngine, "mud": MudEngine}
+const engines = { dojo: DojoEngine, mud: MudEngine };
 
-const coreDefaults = getCoreDefaultsFromUrl()
+const coreDefaults = getCoreDefaultsFromUrl();
 
 if (rootElement) {
-    ReactDOM.createRoot(rootElement).render(
-        <React.StrictMode>
-            <App />
-        </React.StrictMode>
-    )
+	ReactDOM.createRoot(rootElement).render(
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>,
+	);
 } else {
-    console.error("Failed to find the root element")
+	console.error("Failed to find the root element");
 }
--- src/pages/GamePage/GamePage.tsx
+++ src/pages/GamePage/GamePage.tsx
@@ -1,115 +1,111 @@
-import Apps from "@/components/GamePage/Apps/Apps.tsx"
-import SimpleColorPicker from "@/components/GamePage/ColorPicker/SimpleColorPicker.tsx"
-import {clearDomChildren} from "@/utils.ts"
-import {type Coordinate, type QueueItem} from "@pixelaw/core"
-import {usePixelawProvider} from "@pixelaw/react"
-import {useEffect, useMemo, useRef} from "react"
-import styles from "./GamePage.module.css"
-// import dialogStyles from "./dialog.css"
-
-
+import Apps from "@/components/GamePage/Apps/Apps.tsx";
+import SimpleColorPicker from "@/components/GamePage/ColorPicker/SimpleColorPicker.tsx";
+import type {Coordinate, Interaction, QueueItem} from "@pixelaw/core";
+import {InteractionDialog, usePixelawProvider} from "@pixelaw/react";
+import {useEffect, useMemo, useRef, useState} from "react";
+import styles from "./GamePage.module.css";
 
 const GamePage: React.FC = () => {
-    // TODO: Ideally pixelawCore doesnt need to be exposed here, and we have a setter for renderer
-    const { pixelawCore, coreStatus, world, app, color, center, zoom, setColor } = usePixelawProvider()
-    const { viewPort: renderer } = pixelawCore
-    const rendererContainerRef = useRef<HTMLDivElement | null>(null)
-    const dialogContainerRef = useRef<HTMLDivElement | null>(null)
+	// TODO: Ideally pixelawCore doesnt need to be exposed here, and we have a setter for renderer
+	const { pixelawCore, coreStatus, world, app, color, center, zoom, setColor } =
+		usePixelawProvider();
+	const { viewPort: renderer } = pixelawCore;
+	const rendererContainerRef = useRef<HTMLDivElement | null>(null);
+	const [currentInteraction, setCurrentInteraction] =
+		useState<Interaction | null>(null);
 
+	const zoombasedAdjustment = useMemo(() => {
+		return zoom > 3000 ? "1rem" : "-100%";
+	}, [zoom]);
 
-    const zoombasedAdjustment = useMemo(() => {
-        return zoom > 3000 ? "1rem" : "-100%"
-    }, [zoom])
+	// Updating the URL
+	useEffect(() => {
+		const updateURL = () => {
+			const queryParams = new URLSearchParams();
+			queryParams.set("app", app ?? "");
+			queryParams.set("center", `${center[0]},${center[1]}`);
+			queryParams.set("zoom", zoom.toString());
+			queryParams.set("color", color.toString());
+			queryParams.set("world", world!);
+			const newSearch = `?${queryParams.toString()}`;
 
-    // Updating the URL
-    useEffect(() => {
-        const updateURL = () => {
-            const queryParams = new URLSearchParams()
-            queryParams.set("app", app ?? "")
-            queryParams.set("center", `${center[0]},${center[1]}`)
-            queryParams.set("zoom", zoom.toString())
-            queryParams.set("color", color.toString())
-            queryParams.set("world", world!)
-            const newSearch = `?${queryParams.toString()}`
+			if (window.location.search !== newSearch) {
+				window.history.replaceState(null, "", newSearch);
+			}
+		};
+		updateURL();
+	}, [app, center, zoom, color, world]);
 
-            if (window.location.search !== newSearch) {
-                window.history.replaceState(null, "", newSearch)
-            }
-        }
-        updateURL()
+	// Handle viewport events
+	useEffect(() => {
+		const handleCellClick = async (cell: Coordinate) => {
+			const interaction = await pixelawCore.prepInteraction(cell);
 
-    }, [app, center, zoom, color, world])
+			if (interaction.getUserParams().length === 0) {
+				await interaction.execute();
+				return;
+			}
 
+			console.log(interaction);
+			setCurrentInteraction(interaction);
+		};
 
-    // Handle viewport events
-    useEffect(() => {
+		const handleQueueItem = (item: QueueItem) => {
+			pixelawCore.executeQueueItem(item).catch(console.error);
+		};
 
-        const handleCellClick = async (cell: Coordinate) => {
-            if (!dialogContainerRef || !dialogContainerRef.current) return
+		pixelawCore.queue.eventEmitter.on("scheduled", handleQueueItem);
 
-            const interaction = await pixelawCore.handleInteraction(cell)
+		// pixelawCore.events.on("cellHovered", handleCellHover)
+		pixelawCore.events.on("cellClicked", handleCellClick);
 
-            // Clear existing children (dialogs)
-            clearDomChildren(dialogContainerRef.current)
+		return () => {
+			// pixelawCore.events.off("cellHovered", handleCellHover)
+			pixelawCore.events.off("cellClicked", handleCellClick);
+			pixelawCore.queue.eventEmitter.off("scheduled", handleQueueItem);
+		};
+	}, [pixelawCore]);
 
-            // It is possible the interaction has no dialog, like with "paint"
-            // In this case, the onchain action is immediately performed
-            if (interaction.dialog) {
-                // Append the new dialog
-                dialogContainerRef.current.appendChild(interaction.dialog)
+	useEffect(() => {
+		if (coreStatus !== "ready") return;
 
-                interaction.dialog.showModal()
-            }
-        }
+		renderer.setContainer(rendererContainerRef.current!);
+	}, [coreStatus, renderer]);
 
-        const handleQueueItem = (item: QueueItem) => {
-            pixelawCore
-                .executeQueueItem(item)
-                .catch(console.error)
-        }
+	return (
+		<>
+			<div
+				ref={rendererContainerRef}
+				style={{ width: "100%", height: "100%" }}
+			/>
 
+			<div
+				className={styles.colorpicker}
+				style={{ bottom: zoombasedAdjustment }}
+			>
+				<SimpleColorPicker color={color} onColorSelect={setColor} />
+			</div>
 
-        pixelawCore.queue.eventEmitter.on("scheduled", handleQueueItem)
+			<div className={styles.apps} style={{ left: zoombasedAdjustment }}>
+				<Apps />
+			</div>
+			{currentInteraction && (
+				<InteractionDialog
+					interaction={currentInteraction}
+					onSubmit={(interaction) => {
+						console.log("Interaction submitted:", interaction);
+						setCurrentInteraction(null); // Clear the interaction after submission
+					}}
+					onCancel={(interaction) => {
+						console.log("Interaction cancelled:", interaction);
+						setCurrentInteraction(null); // Clear the interaction after submission
+					}}
+				/>
+			)}
+		</>
+	);
 
+	//</editor-fold>
+};
 
-        // pixelawCore.events.on("cellHovered", handleCellHover)
-        pixelawCore.events.on("cellClicked", handleCellClick)
-
-        return () => {
-            // pixelawCore.events.off("cellHovered", handleCellHover)
-            pixelawCore.events.off("cellClicked", handleCellClick)
-            pixelawCore.queue.eventEmitter.off("scheduled", handleQueueItem)
-        }
-    }, [pixelawCore])
-
-
-    useEffect(() => {
-        if (coreStatus !== "ready") return
-
-        renderer.setContainer(rendererContainerRef.current!)
-
-    }, [coreStatus, renderer])
-
-    return (
-        <>
-            <div ref={rendererContainerRef} style={{ width: "100%", height: "100%" }} />
-
-            <div className={styles.colorpicker} style={{ bottom: zoombasedAdjustment }}>
-                <SimpleColorPicker color={color} onColorSelect={setColor} />
-            </div>
-
-            <div className={styles.apps} style={{ left: zoombasedAdjustment }}>
-                <Apps />
-            </div>
-            <div ref={dialogContainerRef}>
-                <dialog>
-                    <p>Dialog content goes here.</p>
-                </dialog>
-            </div>
-        </>
-    )
-
-    //</editor-fold>
-}
-
-export default GamePage
+export default GamePage;
