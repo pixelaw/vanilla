e1f57da8d504c78202d7b63085fffa8df28c6420
thiscaspar <thiscaspar@gmail.com>
2025-04-03 05:17:59 +0800

    Add URL reset functionality to SettingsPage component

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -2,34 +2,36 @@
 import styles from "./SettingsPage.module.css"
 
 const SettingsPage = () => {
-    const handleClearIdb = async () => {
-        // TODO hookup webcore
-        // await clearIdb()
-    }
+	const handleClearIdb = async () => {
+		// TODO hookup webcore
+		// await clearIdb()
+	};
+	const handleResetUrl = () => {
+		const url = window.location.origin; // + window.location.pathname;
+		window.location.href = url;
+	};
+	return (
+		<div className={styles.inner}>
+			<h1>Settings</h1>
+			<ul className={styles.settingsList}>
+				<li className={styles.settingItem}>
+					<span className={styles.settingLabel}>Clear IndexedDB</span>
+					<button type={"button"} onClick={handleClearIdb}>
+						Clear
+					</button>
+				</li>
+				<li className={styles.settingItem}>
+					<span className={styles.settingLabel}>Example Setting</span>
+					<input type="text" className={styles.settingInput} />
+				</li>
+				<li className={styles.settingItem}>
+					<button type={"button"} onClick={handleResetUrl}>
+						Reset URL
+					</button>
+				</li>
+			</ul>
+		</div>
+	);
+};
 
-    return (
-        <div className={styles.inner}>
-            <h1>Settings</h1>
-            <ul className={styles.settingsList}>
-                <li className={styles.settingItem}>
-                    <span className={styles.settingLabel}>Clear IndexedDB</span>
-                    <button
-                        type={"button"}
-                        onClick={handleClearIdb}
-                    >
-                        Clear
-                    </button>
-                </li>
-                <li className={styles.settingItem}>
-                    <span className={styles.settingLabel}>Example Setting</span>
-                    <input
-                        type="text"
-                        className={styles.settingInput}
-                    />
-                </li>
-            </ul>
-        </div>
-    )
-}
-
-export default SettingsPage
+export default SettingsPage;
