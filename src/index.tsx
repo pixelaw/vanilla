877efc8867a82e4646f09066e730a8c00aab8d29
thiscaspar <thiscaspar@gmail.com>
2025-05-31 04:25:19 +0800

    Refactor code for consistency and readability in utils.ts, worlds.json, and index.tsx
    - Update default world to "sepolia" in utils.ts
    - Adjust formatting and spacing across files
    - Update `serverUrl` for "sepolia" in worlds.json
    - Ensure consistent return statements and error handling in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,83 +1,77 @@
-import Main from "@/Main.tsx";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "@/index.css";
-import {PixelawProvider, usePixelawProvider} from "@pixelaw/react";
-import {BrowserRouter} from "react-router-dom";
-import {DojoEngine} from "@pixelaw/core-dojo";
-import {MudEngine} from "@pixelaw/core-mud";
-import {getCoreDefaultsFromUrl, getWorldForUrl} from "@/utils.ts";
-import {StarknetChainProvider} from "@pixelaw/react-dojo";
-import type {WorldsRegistry} from "@pixelaw/core";
-import worldsRegistry from "@/config/worlds.json";
+import Main from "@/Main.tsx"
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "@/index.css"
+import worldsRegistry from "@/config/worlds.json"
+import { DEFAULT_WORLD, getCoreDefaultsFromUrl, getWorldForUrl } from "@/utils.ts"
+import type { WorldsRegistry } from "@pixelaw/core"
+import { DojoEngine } from "@pixelaw/core-dojo"
+import { MudEngine } from "@pixelaw/core-mud"
+import { PixelawProvider, usePixelawProvider } from "@pixelaw/react"
+import { StarknetChainProvider } from "@pixelaw/react-dojo"
+import { BrowserRouter } from "react-router-dom"
 
 const AppContent = React.memo(() => {
-	const { coreStatus } = usePixelawProvider();
+    const { coreStatus } = usePixelawProvider()
 
-	if (coreStatus === "error") {
-		return <div className="error-message">Error occurred, check the logs</div>;
-	}
-	if (coreStatus === "initAccount") {
-		return (
-			<BrowserRouter>
-				<StarknetChainProvider>
-					<div className="loading-message">
-						Pls wait 🧘 : Initializing account
-					</div>
-				</StarknetChainProvider>
-			</BrowserRouter>
-		);
-	}
-	if (coreStatus === "ready" || coreStatus === "readyWithoutWallet") {
+    if (coreStatus === "error") {
+        return <div className="error-message">Error occurred, check the logs</div>
+    }
+    if (coreStatus === "initAccount") {
+        return (
+            <BrowserRouter>
+                <StarknetChainProvider>
+                    <div className="loading-message">Pls wait 🧘 : Initializing account</div>
+                </StarknetChainProvider>
+            </BrowserRouter>
+        )
+    }
+    if (coreStatus === "ready" || coreStatus === "readyWithoutWallet") {
         // TODO This renders many many times, due to "center" changes in GamePage
 
-		return (
-			<BrowserRouter>
-				<StarknetChainProvider>
-					{" "}
-					<Main />
-				</StarknetChainProvider>
-			</BrowserRouter>
-		);
-	}
+        return (
+            <BrowserRouter>
+                <StarknetChainProvider>
+                    {" "}
+                    <Main />
+                </StarknetChainProvider>
+            </BrowserRouter>
+        )
+    }
 
-	return <div className="loading-message">Pls wait 🧘 : Loading</div>;
-});
+    return <div className="loading-message">Pls wait 🧘 : Loading</div>
+})
 
 const App = () => {
-	return (
-		<PixelawProvider
-			worldsRegistry={worldsRegistry as WorldsRegistry}
-			world={world}
-			engines={engines}
-			coreDefaults={coreDefaults}
-		>
-			<AppContent />
-		</PixelawProvider>
-	);
-};
+    return (
+        <PixelawProvider
+            worldsRegistry={worldsRegistry as WorldsRegistry}
+            world={world}
+            engines={engines}
+            coreDefaults={coreDefaults}
+        >
+            <AppContent />
+        </PixelawProvider>
+    )
+}
 
-const { protocol, hostname } = window.location;
-const baseUrl = `${protocol}//${hostname}`;
+const { protocol, hostname } = window.location
+const baseUrl = `${protocol}//${hostname}`
 
-const world = getWorldForUrl(
-	worldsRegistry as WorldsRegistry,
-	baseUrl,
-	"local",
-);
+const world = getWorldForUrl(worldsRegistry as WorldsRegistry, baseUrl, DEFAULT_WORLD)
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById("root")
 
-const engines = { dojo: DojoEngine, mud: MudEngine };
+const engines = { dojo: DojoEngine, mud: MudEngine }
 
-const coreDefaults = getCoreDefaultsFromUrl();
+const coreDefaults = getCoreDefaultsFromUrl()
 
 if (rootElement) {
-	ReactDOM.createRoot(rootElement).render(
-		<React.StrictMode>
-			<App />
-		</React.StrictMode>,
-	);
+    ReactDOM.createRoot(rootElement).render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>,
+    )
 } else {
-	console.error("Failed to find the root element");
+    console.error("Failed to find the root element")
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,49 +1,43 @@
-import {type WorldsRegistry, ZOOM_DEFAULT} from "@pixelaw/core";
+import { type WorldsRegistry, ZOOM_DEFAULT } from "@pixelaw/core"
 
-export const DEFAULT_WORLD = "local";
+export const DEFAULT_WORLD = "sepolia"
 
 export const clearDomChildren = (element: HTMLElement) => {
-	while (element.firstChild) {
-		element.removeChild(element.firstChild);
-	}
-};
+    while (element.firstChild) {
+        element.removeChild(element.firstChild)
+    }
+}
 export const hexRGBtoNumber = (color: string) => {
-	return Number.parseInt(`${color.replace("#", "")}FF`, 16);
-};
+    return Number.parseInt(`${color.replace("#", "")}FF`, 16)
+}
 
 export const numberToHexRGB = (num: number) => {
-	const hex = (num >>> 8).toString(16).padStart(6, "0");
-	return `#${hex}`.toUpperCase();
-};
+    const hex = (num >>> 8).toString(16).padStart(6, "0")
+    return `#${hex}`.toUpperCase()
+}
 
-export function getWorldForUrl(
-	config: WorldsRegistry,
-	url: string,
-	defaultWorld: string,
-): string {
-	for (const worldName in config) {
-		// @ts-ignore TODO
-		if (config[worldName].config.serverUrl.startsWith(url)) {
-			return worldName;
-		}
-	}
+export function getWorldForUrl(config: WorldsRegistry, url: string, defaultWorld: string): string {
+    for (const worldName in config) {
+        // @ts-ignore TODO
+        if (config[worldName].config.serverUrl.startsWith(url)) {
+            return worldName
+        }
+    }
 
-	return defaultWorld;
+    return defaultWorld
 }
 
 export function getCoreDefaultsFromUrl() {
-	const queryParams = new URLSearchParams(window.location.search);
-	const app = queryParams.get("app") || "";
-	const color = Number.parseInt(queryParams.get("color") || "0", 10);
-	const center = queryParams.get("center")?.split(",").map(Number) as
-		| [number, number]
-		| undefined;
-	const zoom = Number.parseFloat(queryParams.get("zoom") || "7");
+    const queryParams = new URLSearchParams(window.location.search)
+    const app = queryParams.get("app") || ""
+    const color = Number.parseInt(queryParams.get("color") || "0", 10)
+    const center = queryParams.get("center")?.split(",").map(Number) as [number, number] | undefined
+    const zoom = Number.parseFloat(queryParams.get("zoom") || "7")
 
-	return {
-		app,
-		color: Number.isNaN(color) ? 0 : color,
-		center: center && center.length === 2 ? center : [12, 12],
-		zoom: Number.isNaN(zoom) ? ZOOM_DEFAULT : zoom,
-	};
+    return {
+        app,
+        color: Number.isNaN(color) ? 0 : color,
+        center: center && center.length === 2 ? center : [12, 12],
+        zoom: Number.isNaN(zoom) ? ZOOM_DEFAULT : zoom,
+    }
 }
